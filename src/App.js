0f1eaa19cb512206e520c944ea6532a09f928994
arseniy_pomazkov <arpomazkov@bk.ru>
2020-07-15 23:06:53 +0300

    Added checkboxes and numbering to list items

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React from 'react';
 import ToDoList from './ToDo/ToDoList'
 
 function App() {
+  let todos = [
+    {
+      id: 1,
+      completed: false,
+      title: 'Погулять с собакой'
+    },
+    {
+      id: 2,
+      completed: false,
+      title: 'Сходить в магазин'
+    },
+    {
+      id: 3,
+      completed: false,
+      title: 'Сделать тренировку'
+    }
+  ]
+
+  function toggleTodo(id) {
+    todos = todos.map((todo) => {
+    if (todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo
+  })
+  }
+
   return (
     <div className='wrapper'>
       <h1>React JS Project</h1>
-      <ToDoList />
+      <ToDoList todos={todos} onToggle={toggleTodo}/>
     </div>
   );
 }
--- src/ToDo/ToDoList.js
+++ src/ToDo/ToDoList.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import ToDoItem from './ToDoItem'
 
-export default function ToDoList() {
-    return (
-    <ul>
-        <li>1</li>
-        <li>1</li>
-    </ul>
-    )
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0
+    }
 }
+
+function ToDoList(props) {
+    return ( < ul style = {
+            styles.ul
+        } > {
+            props.todos.map((todo, index) => {
+                return <ToDoItem todo = {todo}
+                key = {todo.id}
+                index = {index}
+                onChange = {props.onToggle}
+                />
+            })
+        } </ul>)
+    }
+
+    ToDoList.propTypes = {
+        todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+        onToggle: PropTypes.func.isRequired
+    }
+
+    export default ToDoList
