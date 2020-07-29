56f99a4c3454bd8036394ad97a904ed09ea8b7bf
arseniy_pomazkov <arpomazkov@bk.ru>
2020-07-29 12:48:37 +0300

    Added delete/add functions and loading animation

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ToDoList from './ToDo/ToDoList'
+import Context from './context'
+// import AddTodo from './ToDo/AddToDo'
+import Loader from './Loader'
+import Modal from './Modal/Modal'
+
+const AddTodo = React.lazy(() => import('./ToDo/AddToDo'))
 
 function App() {
-  let todos = [
-    {
-      id: 1,
-      completed: false,
-      title: 'Погулять с собакой'
-    },
-    {
-      id: 2,
-      completed: false,
-      title: 'Сходить в магазин'
-    },
-    {
-      id: 3,
-      completed: false,
-      title: 'Сделать тренировку'
-    }
-  ]
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout (() => {
+          setTodos(todos)
+          setLoading(false)
+        }, 2000)
+      })
+  }, [])
+
 
   function toggleTodo(id) {
-    todos = todos.map((todo) => {
-    if (todo.id === id) {
-      todo.completed = !todo.completed
-    }
-    return todo
-  })
+    setTodos(todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }))
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+  function AddToDo(title) {
+    setTodos(todos.concat([{
+      id: Date.now(),
+      completed: false,
+      title: title
+    }]))
   }
 
   return (
+    <Context.Provider value={{ removeTodo }}>
     <div className='wrapper'>
-      <h1>React JS Project</h1>
-      <ToDoList todos={todos} onToggle={toggleTodo}/>
+      <h1> React JS Project </h1>
+      <Modal />
+
+      <React.Suspense fallback={<p>Loading</p>}>
+        <AddTodo onCreate={AddToDo} />
+      </React.Suspense>
+
+      {loading && <Loader />}
+      {todos.length ? (<ToDoList todos = {todos} onToggle = {toggleTodo}/>) : (loading ? null : <p>No todos!</p>)}
     </div>
+    </Context.Provider>
   );
 }
 
--- src/ToDo/ToDoItem.js
+++ src/ToDo/ToDoItem.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import PropTypes from 'prop-types'
+import Context from '../context'
 
 const styles = {
     li: {
@@ -20,18 +21,25 @@ const styles = {
 }
 
 function ToDoItem({todo, index, onChange}) {
-    console.log(todo);
-    return ( <li style={styles.li} >
-    <span>
-        <input type='checkbox' style={styles.input} onChange={() => onChange(todo.id)}/>
+  const {removeTodo} = useContext(Context)
+  const classes = [];
+
+  if (todo.completed) {
+    classes.push('done')
+  }
+
+  return (
+  <li style={styles.li} >
+    <span className={classes.join(' ')}>
+        <input type='checkbox' checked={todo.completed} style={styles.input} onChange={() => onChange(todo.id)}/>
         <strong> {
             index + 1
         } </strong>
         &nbsp;
         {todo.title}
     </span>
-    <button style={styles.button}>&times;</button>
-</li>)
+    <button style={styles.button} onClick={removeTodo.bind(null, todo.id)}>&times;</button>
+  </li>)
 }
 
 ToDoItem.propTypes = {
